00207ee4121868a7c5affc85e9f2bd3512bfe76c
bhushankhopkarr <bhushankhopkar06@gmail.com>
2026-03-18 14:38:38 +0530

    chore(frontend): add frontend/tailwind.config.ts

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        border: "hsl(var(--border-hsl))",
+        input: "hsl(var(--input-hsl))",
+        ring: "hsl(var(--ring-hsl))",
+        background: "hsl(var(--background-hsl))",
+        foreground: "hsl(var(--foreground-hsl))",
+        primary: {
+          DEFAULT: "hsl(var(--primary-hsl))",
+          foreground: "hsl(var(--primary-foreground-hsl))",
+        },
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
